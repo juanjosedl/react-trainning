722611f04663de26acda4d644f18e93627a19950
juanjosedl <jdl.tic@gmail.com>
2023-06-27 21:07:47 -0600

    V3

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Ticnnovacion Consultoría
+          Ticnnovacion Consulting and Services
         </p>
         <a
           className="App-link"
